cf57a3f254979c56852b9f6bd8a873cc38607988
Varsha Devadas <varsha.devadas@gmail.com>
2013-10-30 12:25:42 -0400

    new char sprites

---
+++ Leecher/Leecher/LevelTwo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Leecher
+{
+    class LevelTwo
+    {
+        
+
+    }
+    
+}
