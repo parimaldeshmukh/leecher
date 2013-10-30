6a1dae42c1d45290c644047405e12e5ac186c5b6
parimal <parimal.deshmukh1@gmail.com>
2013-10-30 15:17:14 -0400

    removed comments

--- Leecher/Leecher/Game.cs
+++ Leecher/Leecher/Game.cs
@@ -21,39 +21,33 @@ namespace Leecher
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             this.graphics.IsFullScreen = true;
-            //levelOne = new LevelOne();
-            level = new LevelTwo();
+            level = new LevelOne();
         }
 
         protected override void Initialize()
         {
-            //levelOne.Initialise();
             level.Initialise();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            //levelOne.LoadContent(GraphicsDevice, graphics, Content);
             level.LoadContent(GraphicsDevice, graphics, Content);
         }
 
         protected override void UnloadContent()
         {
-            //levelOne.UnloadContent();
             level.UnloadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            //if (!levelOne.Update(gameTime)) base.Exit();
             if (!level.Update(gameTime)) base.Exit();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            //levelOne.Draw(GraphicsDevice);
             level.Draw(GraphicsDevice);
             base.Draw(gameTime);
         }
